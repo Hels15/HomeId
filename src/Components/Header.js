0454450ad21eaa3d7bc95f6a753b22baa1a6900c
Hadházy Tamás <helsgithub15@gmail.com>
2022-02-03 18:05:56 +0100

    select bug

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,9 @@ import Select from '@mui/material/Select';
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 
+
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 import LanguageIcon from '@mui/icons-material/Language';
 
 function MenuHeaderItem({item_data, onClick}){
@@ -60,41 +63,74 @@ function Menu(){
 }
 
 function Header(item_data) {
-    const [input_label_text, set_input_label_text] = useState("Test")
+    const [input_label_text, set_input_label_text] = useState("lang")
     const [input_label_icon, set_input_label_icon] = useState(true)
     const [input_label_subtitle, set_input_label_subtitle] = useState(false)
     const [input_language, set_input_language] = useState("")
+    const [languages_display, set_languages_display] = useState(true)
+    const [hu, set_hu] = useState("hu")
+    const [en, set_en] = useState("en")
+
+
     const handleSelectValue = (e) =>{
+        set_en("en")
+        set_hu("hu")
         set_input_language(e.target.value)
         set_input_label_text("")
         set_input_label_icon("none")
         set_input_label_subtitle(true)
+        set_languages_display(true)
     }
     const handleLanguages = (lang) =>{
         set_input_language(lang)
+
     }
+    const handleIconClick = (e) => {
+        set_en("")
+        set_hu("")
+        set_input_language("")
+        set_input_label_text("lang")
+        set_input_label_icon("visible")
+        set_input_label_subtitle(false)
+    }
+
+
+
     return (
        <div className="header-container">
            <div className="content-base">
                 <div className="test-header">
                     <img src={logo} alt=""/>
                     <div><Menu/></div>
-                    <FormControl sx={{ "&:hover":{
+                    <FormControl
+                        sx={{ filter:"drop-shadow(10px, 10px, 10px, #141414)","&:hover":{
                         filter:"drop-shadow(10px, 10px, 10px, black)"
                         }}}>
-
-                        <InputLabel shrink="true" sx={{top: "45px", fontSize: "20px"}}>
+                        <div className="input-label">
+                             <InputLabel shrink="true" sx={{top: "45px",fontSize: "20px"}}>
                             {input_label_text}
-                            <LanguageIcon  sx={{width: "auto",marginLeft: "4px",display: input_label_icon}}/>
+                            <LanguageIcon  sx={{marginLeft: "4px",display: input_label_icon}}/>
+
                         </InputLabel>
-                        <Select  onChange={(e) => handleSelectValue(e)} sx={{width: "100px", borderRadius: "10px", marginTop: '20px'
+                        </div>
+
+                        <Select onChange={(e) => handleSelectValue(e)} sx={{"& .MuiSelect-nativeInput":{
+                        width: 0},"& .MuiMenuItem-root":{
+                            display: "none"
+                            },width: "auto",borderRadius: "10px", marginTop: '20px'
 
                             }}>
-                            <MenuItem  value="hu">hu</MenuItem>
-                            <MenuItem  value="en">en</MenuItem>
+                            <MenuItem id="hun"  value="hu">{hu}</MenuItem>
+                            <MenuItem  id="eng" value="en">{en}</MenuItem>
 
                         </Select>
-                        <p style={{display:input_label_subtitle? "block": "none", filter: "drop-shadow(5px 5px 2px rgba(0,0,0, 0.1))"}}>Selected language <span style={{color: "#4491b8", fontWeight:"700"}}>{input_language}</span> </p>
+                        <div className="flex-icons">
+                            <p style={{position: "absolute",display:input_label_subtitle? "block": "none", filter: "drop-shadow(5px 5px 2px rgba(0,0,0, 0.1))", width: "200px", right: "-25px",top: "5px"}}>Selected language <span style={{color: "#4491b8", fontWeight:"700"}}>{input_language}</span> </p>
+                         <IconButton onClick={handleIconClick} sx={{display: input_label_subtitle? "block": "none", top: "-2px", left: "114px", marginLeft: "5px"}}>
+                                <DeleteIcon/>
+                            </IconButton>
+                        </div>
+
                     </FormControl>
 
                 </div>
